ca055bf7ebbadc3a5a26b9271acfd4bcc7795a5d
lavabyte <buweiliao@gmail.com>
2024-03-14 11:29:26 +0100

    🗒 export singletoned db instance

--- app/utils/singleton.server.ts
+++ app/utils/singleton.server.ts
@@ -1,8 +1,14 @@
 // Borrowed & modified from https://github.com/jenseng/abuse-the-platform/blob/main/app/utils/singleton.ts
 
 export const singleton = <Value>(name: string, valueFactory: () => Value): Value => {
+	// 🧠 global is a reserved variable in node.js
 	const g = global as any
+
+	// check if the singleton is already created
 	g.__singletons ??= {}
+
+	// check if it has a name property
 	g.__singletons[name] ??= valueFactory()
+
 	return g.__singletons[name]
 }
