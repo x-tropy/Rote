c263aca59fdf9570c07d0f18053fa147529c7008
lavabyte <buweiliao@gmail.com>
2024-03-15 17:52:20 +0100

    🔥 remove useless stuff

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,31 +16,5 @@ export const links: LinksFunction = () => [
 ]
 
 export default function Index() {
-	return (
-		<div className='text-left'>
-			<h1 className='underline text-lime-600'>Welcome to Remix</h1>
-			<p className='crazy'>
-				consectetur adipiscing elit. Sed id consectetur justo. Nullam posuere metus vitae quam consectetur, id bibendum elit pellentesque. Vivamus auctor sodales tortor, nec volutpat ligula.
-				Suspendisse potenti. Sed eu lectus nec augue consequat fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse potenti. Quisque luctus,
-				nunc eu eleifend ullamcorper, justo purus tristique velit, a tristique odio odio vel augue.
-			</p>
-			<ul>
-				<li>
-					<a target='_blank' href='https://remix.run/tutorials/blog' rel='noreferrer'>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					</a>
-				</li>
-				<li>
-					<a target='_blank' href='https://remix.run/tutorials/jokes' rel='noreferrer'>
-						D Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-						Remix Docs
-					</a>
-				</li>
-			</ul>
-		</div>
-	)
+	return <h1 className='text-2xl'>Index</h1>
 }
