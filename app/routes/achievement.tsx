0d65db57d15494542da57e59f53483da9ab60941
lavabyte <buweiliao@gmail.com>
2024-03-19 11:57:05 +0100

    📄 add new empty routes

---
+++ app/routes/achievement.tsx
@@ -0,0 +1,9 @@
+import { Main } from "~/components/Container"
+
+export default function AchievementRoute() {
+	return (
+		<Main>
+			<h1>Achievement</h1>
+		</Main>
+	)
+}
---
+++ app/routes/discover.tsx
@@ -0,0 +1,9 @@
+import { Main } from "~/components/Container"
+
+export default function DiscoverRoute() {
+	return (
+		<Main>
+			<h1>Discover</h1>
+		</Main>
+	)
+}
