eb51191bb36b37493ee63577690195022a217918
lavabyte <buweiliao@gmail.com>
2024-03-21 20:11:57 +0100

    🚚 move NoAccessState to /component/SpiceUp

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,5 +1,5 @@
-import { Card, Elevation, NonIdealState, Button, Divider, ButtonGroup } from "@blueprintjs/core"
-import { Link } from "@remix-run/react"
+import { Card, Elevation } from "@blueprintjs/core"
+import { NoAccessState } from "~/components/SpiceUp"
 import React, { ReactNode } from "react"
 
 interface MainContainerProps {
@@ -8,16 +8,13 @@ interface MainContainerProps {
 	columns?: number
 }
 
-const CLASS_SINGLE_COLUMN = "my-20 mx-auto w-[700px]"
-const CLASS_TWO_COLUMNS = "my-20 mx-auto w-[800px] grid grid-cols-2 gap-4"
-
 export const Main: React.FC<MainContainerProps> = ({ children, noAccess, columns = 1 }) => {
 	return noAccess || columns === 1 ? (
-		<Card className={CLASS_SINGLE_COLUMN} elevation={Elevation.TWO}>
+		<Card className={"my-20 mx-auto w-[800px]"} elevation={Elevation.TWO}>
 			{!noAccess ? children : NoAccessState}
 		</Card>
 	) : (
-		<div className={CLASS_TWO_COLUMNS}>
+		<div className={"my-20 mx-auto w-[900px] grid grid-cols-2 gap-4"}>
 			{children.map((child, index) => (
 				<Card key={index} elevation={Elevation.TWO}>
 					{child}
@@ -26,28 +23,3 @@ export const Main: React.FC<MainContainerProps> = ({ children, noAccess, columns
 		</div>
 	)
 }
-
-const NoAccessState = (
-	<NonIdealState
-		className='py-40'
-		title='Access Denied'
-		description={
-			<div>
-				You do not have access to this page.
-				<br />
-				Please login or register first.
-			</div>
-		}
-		icon='blocked-person'
-		action={
-			<ButtonGroup vertical={false} className='flex flex-row space-x-4'>
-				<Link to={"/login"}>
-					<Button text='Login' icon='log-in' intent='none' />
-				</Link>
-				<Link to={"/register"}>
-					<Button text='Register' icon='new-person' intent='none' />
-				</Link>
-			</ButtonGroup>
-		}
-	/>
-)
