26ff5a064d0833764874757bb0073aeaf4a5625a
lavabyte <buweiliao@gmail.com>
2024-03-26 14:22:28 +0100

    🥅 wrapper function

--- app/utils/request.server.ts
+++ app/utils/request.server.ts
@@ -1,6 +1,6 @@
 import { json } from "@remix-run/node"
 
-type StatusType = 400 | 401 | 403 | 404
+type StatusType = 400 | 401 | 403 | 404 | 500 | 501 | 502 | 503 | 504 | 505
 
 // ※ 包装函数，加上了状态码
 export function badRequest<T>(data: T, status?: StatusType) {
